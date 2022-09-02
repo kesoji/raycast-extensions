49e7d21449a29825a6892bd83100eb32797bc5c5
kesoji <6465251+kesoji@users.noreply.github.com>
2022-09-02 13:53:28 +0900

    feat: can choose quoting

--- sql-where-in-quoting/src/index.tsx
+++ sql-where-in-quoting/src/index.tsx
@@ -5,20 +5,29 @@ type Values = {
 };
 
 export default function Command() {
-  async function handleSubmit(values: Values) {
+  async function handleSubmit(values: Values, quote = true) {
     const str = values.textarea;
     console.log(str);
     if (!str) {
       showToast({
         style: Toast.Style.Failure,
-        title: 'List is empty.'
-      })
-      return
+        title: "List is empty.",
+      });
+      return;
     }
-    const list = str.replace("\r", "").split("\n")
-    const converted = list.map(s => `"${s}",`).join("\n").slice(0, -1)
-    await Clipboard.copy(converted)
-    
+    const list = str.replace("\r", "").split("\n");
+    const converted = list
+      .map((s) => {
+        if (quote) {
+          return `"${s}",`;
+        } else {
+          return `${s},`;
+        }
+      })
+      .join("\n")
+      .slice(0, -1);
+    await Clipboard.copy(converted);
+
     showToast({ title: "Copied!", message: "Converted list is copied to your clipboard." });
   }
 
@@ -27,6 +36,11 @@ export default function Command() {
       actions={
         <ActionPanel>
           <Action.SubmitForm onSubmit={handleSubmit} title="Convert" />
+          <Action.SubmitForm
+            onSubmit={(values: Values) => handleSubmit(values, false)}
+            title="Convert without quoting"
+            shortcut={{ modifiers: ["cmd", "shift"], key: "enter" }}
+          />
         </ActionPanel>
       }
     >
